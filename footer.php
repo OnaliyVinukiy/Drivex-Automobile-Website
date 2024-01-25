d579588c959ffb833b0712d4a3e495fcdba0f197
Onaliy Vinukiy Jayawardana <ovjayawardana@students.nsbm.ac.lk>
2024-01-25 21:01:55 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,65 @@
+<html>
+    <head>
+        <style>
+            footer {
+    background: linear-gradient(0deg, rgba(8, 32, 50, 0.9), rgba(8, 32, 50, 0.9)), url(image/foot.jpg), #082032;;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+
+footer .footer-top {
+    padding-top: 80px;
+    padding-bottom: 40px;
+}
+
+.footer-bottom {
+    border-top: 1px solid rgba(255, 255, 255, 0.1);
+    padding-bottom: 20px;
+    padding-top: 20px;
+}
+
+footer .navbar-brand {
+    color: #fff;
+}
+
+footer p {
+    color: #ADB3B9;
+}
+
+footer .social-icons a {
+    width: 80px;
+    height: 80px;
+    font-size: 30px;
+    margin-left: 4px;
+    margin-right: 4px;
+}
+</style>
+</head>
+<body>
+    <footer>
+        <div class="footer-top text-center">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 text-center">
+                        <h4 class="navbar-brand">DRIVEX<span class="dot">.</span></h4>
+                        <p>We provide information about all types of cars in the world to all car addictors. We provide information about classic cars, luxury cars, sports cars, electric cars and new arrivals. Also we have a range of articles about cars as well as our social media pages provide you additional insights about all types of cars.</p>
+                        <div class="col-auto social-icons">
+                            <a href="#"><i class='fa fa-facebook-f'></i></a>
+                            <a href="#"><i class='fa fa-twitter'></i></a>
+                            <a href="#"><i class='fa fa-instagram'></i></a>
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom text-center">
+            <p class="mb-0"></p> Made With <i class="fa fa-heart-o" aria-hidden="true"></i> By ICONS<br>
+            <p class ="mb-0"><a href="admin/signup.php" target="_blank">Admin Login</a></p>
+                
+        </div>
+    </footer>
+</body>
+    </html>

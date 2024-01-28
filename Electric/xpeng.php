e57ba34ab4f4cf33a955cd5ba60d8f776b8d9506
Onaliy Vinukiy Jayawardana <ovjayawardana@students.nsbm.ac.lk>
2024-01-28 20:27:58 +0530

    Update xpeng.php

--- Electric/xpeng.php
+++ Electric/xpeng.php
@@ -1,1 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> Xpeng Electric cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
 
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+
+	<header class="header">
+<?php include "header.php"?>
+
+</header>
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Xpeng Electric cars </center></div>
+						<center class="headline">M&nbspa&nbspd&nbspe&nbsp&nbsp i&nbspn&nbsp&nbsp C&nbsph&nbspi&nbspn&nbspa&nbsp</center>
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="https://www.electrive.com/wp-content/uploads/2020/04/xpeng-motors-xpeng-p7-china-2020-05-min-888x444.png">
+				<ul class="post-tags">
+					<li>$37,495⁠</li>
+					<li>2020</li>
+					
+				</ul>
+				<h4 class="headline">XEPNG P7</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbspPerment magnet motor </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp196kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp80.87kwh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspModular single-speed gear</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp170km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Body type :  </td>
+							<td class="descrip">&nbsp four door sedan,five seats</td>
+						</tr>
+						<tr>
+							<td class="topic">Range :  </td>
+							<td class="descrip">&nbsp706km (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="https://www.carscoops.com/wp-content/uploads/2021/04/2022-XPeng-P5-1024x555.jpg">
+				<ul class="post-tags">
+					<li>$105,000</li>
+					<li>2020</li>
+				</ul>
+				<h4 class="headline">XPENG P5</h4>
+				<p>
+                    <table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbspPerment magnet motor</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity: </td>
+							<td td class="descrip">&nbsp155kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp71.4kWh</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp170km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Body type :  </td>
+							<td class="descrip">&nbsp4 Door SUV with two falcon doors</td>
+						</tr>
+						<tr>
+							<td class="topic">Range :  </td>
+							<td class="descrip">&nbsp372 miles (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+				</p>
+				<br><br>
+			</div>	
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="https://i0.wp.com/pevme.com/wp-content/uploads/2021/01/xpeng-g3.jpg?w=2466&ssl=1">
+				<ul class="post-tags">
+					<li>$23,120</li>
+					<li>2021</li>
+				</ul>
+				<h4 class="headline">XPENG G3i</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbspPerment magnet motor</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp145KW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp47.6kWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspDual speed</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp170km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Body type :  </td>
+							<td class="descrip">&nbspSUV five seats </td>
+						</tr>
+						<tr>
+							<td class="topic">Range :  </td>
+							<td class="descrip">&nbsp300km (EPA est.)</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>		
+			<div class="col-lg-5 col-md-5 single-blog"> 
+				<img class="img-fluid" src="https://blogs.nvidia.com/wp-content/uploads/2021/11/xpeng_g9.jpg">
+				<ul class="post-tags">
+					<li>$49,990</li>
+					<li>2017</li>
+				
+				</ul>
+				<h4 class="headline">XPENG G9</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Motor : </td>
+							<td td class="descrip">&nbsp175kw and 230kW</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Power train Capacity : </td>
+							<td td class="descrip">&nbsp405kW(Dual motor Compbine) </td>
+						</tr>
+
+						<tr>
+							<td class="topic">Battery:  </td>
+							<td class="descrip">&nbsp78.2KWh</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbspSingle speed direct drive</td>
+						</tr>
+					
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp124mph</td>
+						</tr>
+						<tr>
+							<td class="topic">Body type :  </td>
+							<td class="descrip">&nbspSUV seven seated</td>
+						</tr>
+				
+						<tr>
+							<td class="topic">Range :  </td>
+							<td class="descrip">&nbsp200km</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				</div>	
+                
+			
+		
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<footer>
+    <div class="footer-top text-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 text-center">
+                    <h4 class="navbar-brand">DRIVEX</h4>
+                    <p>We provide information about all types of cars in the world to all car addictors. We provide information about classic cars, luxury cars, sports cars and new arrivals. Also we have a range of articles about cars as well as our social media pages provide you additional insights about all types of cars.</p>
+                    <div class="col-auto social-icons">
+                        <a href="#"><i class='fa fa-facebook-f'></i></a>
+                        <a href="#"><i class='fa fa-twitter'></i></a>
+                        <a href="#"><i class='fa fa-instagram'></i></a>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom text-center">
+        <p class="mb-0"></p> Made With <i class="fa fa-heart-o" aria-hidden="true"></i> By ICONS
+            
+    </div>
+</footer>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+</body>

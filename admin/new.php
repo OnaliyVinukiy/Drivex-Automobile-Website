8c9469a94e897fe42ff76f0873f8dd4de1b4417e
Onaliy Vinukiy Jayawardana <ovjayawardana@students.nsbm.ac.lk>
2024-02-03 22:06:23 +0530

    Create new.php

---
+++ admin/new.php
@@ -0,0 +1,660 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>new arrivals</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Travelix Project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/luxurycars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+
+
+		<!-- Main Navigation -->
+
+		
+		<nav class="main_nav">
+			<div class="container">
+				<div class="row">
+					<div class="col main_nav_col d-flex flex-row align-items-center justify-content-start">
+						<div class="logo_container">
+							<div class="logo"><a href="#"><img src="../image/Drivex.png" alt=""></a></div>
+						</div>
+						<div class="main_nav_container ml-auto">
+							<ul class="main_nav_list">
+								<li class="main_nav_item"><a href="../index.php">home</a></li>
+								<li class="main_nav_item"><a href="../about.php">about us</a></li>
+								<li class="main_nav_item"><a href="../blog.php">blog</a></li>
+								<li class="main_nav_item"><a href="../index.php#contact">feedback</a></li>
+								<li class="main_nav_item"><a href="../contact.php">contact us</a></li>
+							</ul>
+						</div>
+						<div >
+							
+								<g>
+									<g>
+										<g>
+											<path class="mag_glass" fill="#FFFFFF" d="M78.438,216.78c0,57.906,22.55,112.343,63.493,153.287c40.945,40.944,95.383,63.494,153.287,63.494
+											s112.344-22.55,153.287-63.494C489.451,329.123,512,274.686,512,216.78c0-57.904-22.549-112.342-63.494-153.286
+											C407.563,22.549,353.124,0,295.219,0c-57.904,0-112.342,22.549-153.287,63.494C100.988,104.438,78.439,158.876,78.438,216.78z
+											M119.804,216.78c0-96.725,78.69-175.416,175.415-175.416s175.418,78.691,175.418,175.416
+											c0,96.725-78.691,175.416-175.416,175.416C198.495,392.195,119.804,313.505,119.804,216.78z"/>
+										</g>
+									</g>
+									<g>
+										<g>
+											<path class="mag_glass" fill="#FFFFFF" d="M6.057,505.942c4.038,4.039,9.332,6.058,14.625,6.058s10.587-2.019,14.625-6.058L171.268,369.98
+											c8.076-8.076,8.076-21.172,0-29.248c-8.076-8.078-21.172-8.078-29.249,0L6.057,476.693
+											C-2.019,484.77-2.019,497.865,6.057,505.942z"/>
+										</g>
+									</g>
+								</g>
+							</svg>
+						</div>
+
+						
+
+						<div class="hamburger">
+							<i class="fa fa-bars trans_200"></i>
+						</div>
+					</div>
+				</div>
+			</div>	
+		
+		</nav>
+
+	</header>
+
+	<div class="menu trans_500">
+		<div class="menu_content d-flex flex-column align-items-center justify-content-center text-center">
+			<div class="menu_close_container"><div class="menu_close"></div></div>
+			<div class="logo menu_logo"><a href="#"><img src="../image/logo.png" alt=""></a></div>
+			<ul>
+				<li class="main_nav_item"><a href="../index.php">home</a></li>
+				<li class="main_nav_item"><a href="../about.php">about us</a></li>
+				<li class="main_nav_item"><a href="../blog.php">blog</a></li>
+				<li class="main_nav_item"><a href="../index.php#contact">feedback</a></li>
+				<li class="main_nav_item"><a href="../contact.php">contact us</a></li>
+			</ul>
+		</div>
+	</div>
+
+	
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>NEW ARRIVED CARS</center></div>
+						
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-4 col-md-4 single-blog">
+				<img class="img-fluid" src="../image/Chevrolet Z06.jpeg" alt="">
+				<ul class="post-tags">
+					<li>$270,000</li>
+					
+				</ul>
+				<h4 class="headline">Chevrolet Corvette Z06</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp5.5litre V8</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp7000cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Torque :  </td>
+							<td class="descrip">&nbsp 460</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp M1L 8 speed dual clutch</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 70L</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 195mph</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">horsepower :  </td>
+							<td class="descrip">&nbsp 670hp</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Width :  </td>
+							<td class="descrip">&nbsp 2025mm</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 2025mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+            <div class="col-lg-4 col-md-4 single-blog">
+				<img class="img-fluid" src="../image/AUDI SQ8.jpeg" alt="">
+				<ul class="post-tags">
+					<li>$270,000</li>
+					
+				</ul>
+				<h4 class="headline">2024 Audi SQ8 E-TRON</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp4.0 liter twin turbo</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp 496hp</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Torque :  </td>
+							<td class="descrip">&nbsp 568</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 8 speed automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 85L</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 210mph</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						<tr>
+							<td class="topic">Horsepower :  </td>
+							<td class="descrip">&nbsp 496hp</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Width :  </td>
+							<td class="descrip">&nbsp 78.6 inches</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 197.1 inch</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+			<div class="col-lg-4 col-md-4 single-blog">
+				<img class="img-fluid" src="../image/bmw i7.jpeg" alt="">
+				<ul class="post-tags">
+					<li>$270,000</li>
+					
+				</ul>
+				<h4 class="headline">BMW i7 2023</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp 3.0 liter</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp 3000cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Torque :  </td>
+							<td class="descrip">&nbsp 383</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp single speed automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 74L</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 149mph</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">horsepower :  </td>
+							<td class="descrip">&nbsp 375</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Width :  </td>
+							<td class="descrip">&nbsp 86.3 inches</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 212.3 inches</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+			<div class="col-lg-4 col-md-4 single-blog">
+				<img class="img-fluid" src="../image/benz s63.jpeg" alt="">
+				<ul class="post-tags">
+					<li>$270,000</li>
+					
+				</ul>
+				<h4 class="headline">Mercedes AMG S63</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp twin turbo v8</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp 4000cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Torque :  </td>
+							<td class="descrip">&nbsp 1055</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp  9 speed dual clutch automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 80L</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 180mph</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						<tr>
+							<td class="topic">horsepower :  </td>
+							<td class="descrip">&nbsp 192hp</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Width :  </td>
+							<td class="descrip">&nbsp 75.6 inches</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 210.2 inch</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>				
+			
+			
+			<div class="col-lg-4 col-md-4 single-blog">
+				<img class="img-fluid" src="../image/honda accord.jpeg" alt="">
+				<ul class="post-tags">
+					<li>$270,000</li>
+					
+				</ul>
+				<h4 class="headline">2023 Honda Accord 6</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp 1.2 litre turbo charged</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp 1500cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Torque :  </td>
+							<td class="descrip">&nbsp 247</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp CVT</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 60L</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 200kmph</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						<tr>
+							<td class="topic">horsepower :  </td>
+							<td class="descrip">&nbsp 192</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Width :  </td>
+							<td class="descrip">&nbsp 73.3 inches</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 195.7 inch</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+			
+			
+			<div class="col-lg-4 col-md-4 single-blog">
+				<img class="img-fluid" src="../image/BMW M2.jpeg" alt="">
+				<ul class="post-tags">
+					<li>$63,195</li>
+					
+				</ul>
+				<h4 class="headline">BMW M2</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp 3.0 litre twinturbo</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp 3000cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Torque :  </td>
+							<td class="descrip">&nbsp 406</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp six speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 52L</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 155mph</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						<tr>
+							<td class="topic">horseppower :  </td>
+							<td class="descrip">&nbsp 453hp</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Width :  </td>
+							<td class="descrip">&nbsp 74.3 inches</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 180.3 inch</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+            <div class="col-lg-4 col-md-4 single-blog">
+				<img class="img-fluid" src="../image/tesla.jpeg" alt="">
+				<ul class="post-tags">
+					<li>$200,000</li>
+					
+				</ul>
+				<h4 class="headline">Tesla Roadster 2023</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp 5.2 iter v10</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Torque :  </td>
+							<td class="descrip">&nbsp 885</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp </td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp </td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 250mph</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">horsepower :  </td>
+							<td class="descrip">&nbsp 1341hp</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Width :  </td>
+							<td class="descrip">&nbsp 76.4 inches</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 155.4 inch</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+            <div class="col-lg-4 col-md-4 single-blog">
+				<img class="img-fluid" src="../image/audi r8.jpeg" alt="">
+				<ul class="post-tags">
+					<li>$270,000</li>
+					
+				</ul>
+				<h4 class="headline">Audi R8 2023</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp5.2litre V10</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp 5200cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Torque :  </td>
+							<td class="descrip">&nbsp 406</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 speed automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 75L</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 199mph</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Horsepower :  </td>
+							<td class="descrip">&nbsp 562hp</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Width :  </td>
+							<td class="descrip">&nbsp 76.4 inches</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 76.4 inch</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+            <div class="col-lg-4 col-md-4 single-blog">
+				<img class="img-fluid" src="../image/ford.jpeg" alt="">
+				<ul class="post-tags">
+					<li>$1,700,000</li>
+					
+				</ul>
+				<h4 class="headline">2023 Ford GT</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp 3.5 litre twin turbo</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp 3500cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Torque :  </td>
+							<td class="descrip">&nbsp 550</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 speed dua clutch automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 66L</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 216mph</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Horseower :  </td>
+							<td class="descrip">&nbsp 800hp</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">width :  </td>
+							<td class="descrip">&nbsp 78.9 inches</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 187.5 inches</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+		</div>
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<?php include "../footer.php" ?>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+</body>
+
+</html>

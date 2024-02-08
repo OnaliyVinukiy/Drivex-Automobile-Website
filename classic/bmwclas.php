cd4519e270160deb0455221429f31669e0226676
Onaliy Vinukiy Jayawardana <ovjayawardana@students.nsbm.ac.lk>
2024-02-08 21:11:06 +0530

    Create bmwclas.php

---
+++ classic/bmwclas.php
@@ -0,0 +1,432 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> BMW Classic Cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	
+	<!-- Header -->
+
+	<header class="header">
+<?php include "headerclss.php"?>
+
+</header>
+
+	
+<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>BMW Classic Cars</center></div>
+						
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clbmw33.jpg" alt="">
+				<ul class="post-tags">
+					<li>$113,877</li>
+					<li>1933</li>
+					<li>Germany</li>
+					
+				</ul>
+				<h4 class="headline">BMW 303 </h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp1.2l V6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1173cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Gasoline</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 4 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 35l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 90km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 820kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 3900mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clbmw78.jpg" alt="">
+				<ul class="post-tags">
+					<li>$250,000</li>
+					<li>1978</li>
+					<li>Italy</li>
+				</ul>
+				<h4 class="headline">BMW M1</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.5l I6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3453cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol(Gasoline)</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 5 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 116l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 265km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 2 </td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1300kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4361mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>	
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clbmw86.jpg" alt="">
+				<ul class="post-tags">
+					<li>$57,343</li>
+					<li>1986</li>
+					<li>Germany</li>
+				</ul>
+				<h4 class="headline">BMW M3</h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp2.5L I4</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp6240cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Premium Gasoline </td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 5 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 60l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 235km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1360kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4345mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>		
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clbmw62.jpg" alt="">
+				<ul class="post-tags">
+					<li>$78,400 </li>
+					<li>1962</li>
+					<li>Germany</li>
+				</ul>
+				<h4 class="headline">BMW New Class</h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp1.5l V6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp1990cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 5 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 55l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 250km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1403kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4500mm</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				</div>	
+                
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clbmw3200cs.jpg" alt="">
+				<ul class="post-tags">
+					<li>$126,500</li>
+					<li>1962</li>
+					<li>Germany</li>
+				</ul>
+				<h4 class="headline">BMW 3200CS</h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.8L V8</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3169cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol </td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 4 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 75l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 200km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1500kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4850mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>		
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/clbmw57.webp" alt="">
+				<ul class="post-tags">
+					<li>$2,129,800 </li>
+					<li>1955</li>
+					<li>Germany</li>
+				</ul>
+				<h4 class="headline">BMW 507</h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.2L V8</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3168cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 4 Speed Manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 66l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 250km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 2</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1330kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4380mm</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				</div>	
+		</div>
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<?php include "footer.php" ?>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+</body>
+
+</html>

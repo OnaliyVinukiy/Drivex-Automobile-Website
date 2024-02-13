903d705b6739357c0c08f0f17c49277378fd722b
Onaliy Vinukiy Jayawardana <ovjayawardana@students.nsbm.ac.lk>
2024-02-13 18:54:20 +0530

    Create porschesp.php

---
+++ sports/porschesp.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Porsche Sport Cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+
+
+		<!-- Main Navigation -->
+
+		<?php include "header.php"?>
+</header>
+
+	
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Porsche Sport Cars</center></div>
+						
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/spporsche718.jpg">
+				<ul class="post-tags">
+					<li>$62,600</li>
+					<li>2019</li>
+					<li>Germany</li>
+
+				</ul>
+				<h4 class="headline">Porsche 718 Cayman </h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp2.0l V6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3995cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp520NM@5000rpm</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Premium Unleaded Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 261hp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 261km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp4.7s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Carbon-fibre & Titanium</td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1415kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4726mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/spporsche911.jpg">
+				<ul class="post-tags">
+					<li>$162,450</li>
+					<li>2022</li>
+					<li>Germany</li>
+					
+				</ul>
+				<h4 class="headline">Porsche 911 GT3</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp4.0l V6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3996cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp670Nm@4100rpm</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 Speed manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 502hp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 319km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp3.4s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Carbon-fibre & Titanium</td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1418kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4573mm</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				
+			</div>						
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/spporsche718box.jpg">
+				<ul class="post-tags">
+					<li>$62,600</li>
+					<li>2022</li>
+					<li>Germany</li>
+					
+				</ul>
+				<h4 class="headline">Porsche 718 Boxster</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.0l f4</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3995cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp380Nm@6500rpm</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 6 speed manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 350hp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 275km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp4.9s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Carbon-fibre & Titanium</td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1335kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4456mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/porsche962.jpeg">
+				<ul class="post-tags">
+					<li>$46,999</li>
+					<li>2019</li>
+					<li>Germany</li>
+					
+				</ul>
+				<h4 class="headline">Porsche 962 C</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.4L f6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp2649cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp630Nm@8600rpm</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 6 Speed manual</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 620bhp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 350km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp2.8s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Carbon-fibre & Titanium</td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 820kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4800mm</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				
+			</div>	
+			
+			
+				
+		</div>
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<footer>
+    <div class="footer-top text-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 text-center">
+                    <h4 class="navbar-brand">DRIVEX</h4>
+                    <p>We provide information about all types of cars in the world to all car addictors. We provide information about classic cars, luxury cars, sports cars and new arrivals. Also we have a range of articles about cars as well as our social media pages provide you additional insights about all types of cars.</p>
+                    <div class="col-auto social-icons">
+                        <a href="#"><i class='fa fa-facebook-f'></i></a>
+                        <a href="#"><i class='fa fa-twitter'></i></a>
+                        <a href="#"><i class='fa fa-instagram'></i></a>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom text-center">
+        <p class="mb-0"></p> Made With <i class="fa fa-heart-o" aria-hidden="true"></i> By ICONS
+            
+    </div>
+</footer>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+</body>
+
+</html>

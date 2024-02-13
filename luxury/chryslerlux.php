383d6277e3c401e6fa838cdbae9a849e7a4ff825
Onaliy Vinukiy Jayawardana <ovjayawardana@students.nsbm.ac.lk>
2024-02-13 18:57:38 +0530

    Create chryslerlux.php

---
+++ luxury/chryslerlux.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Chrysler Luxury Cars</title>
+<title>Luxury Cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/luxurycars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+<style>
+	<? php include "../styles/luxurycars.css" ?>
+	<? php include "../styles/about_responsive.css" ?>
+</style>	
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+<?php include "headerlux.php"?>
+
+</header>
+
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Chrysler Luxury Cars</center></div>
+						
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-4 col-md-4 single-blog">
+				<img class="img-fluid" src="../image/chy22.jpg" alt="">
+				<ul class="post-tags">
+					<li>$33,545</li>
+					
+				</ul>
+				<h4 class="headline">Chrysler 300 2022</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.6l V6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3605cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Regular Unleaded Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 8 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 70l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 210km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 5</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1855kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 5044mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-4 col-md-4 single-blog">
+				<img class="img-fluid" src="../image/chy21.jpg" alt="">
+				<ul class="post-tags">
+					<li>$33,545</li>
+					
+				</ul>
+				<h4 class="headline">Chrysler 300 2021</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.6l V6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3605cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Premium Unleaded Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 8 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 73l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 265km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 5</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1840kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 5066mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>	
+			<div class="col-lg-4 col-md-4 single-blog">
+				<img class="img-fluid" src="../image/chy20.jpg" alt="">
+				<ul class="post-tags">
+					<li>$31,535</li>
+					
+				</ul>
+				<h4 class="headline">Chrysler 300 2020</h4>
+				<p>
+					<table>
+						
+
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.6l V6</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3605cc</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Premium Unleaded Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 8 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Fuel Tank Capacity :  </td>
+							<td class="descrip">&nbsp 72l</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 224km/h</td>
+						</tr>
+						<tr>
+							<td class="topic">Doors :  </td>
+							<td class="descrip">&nbsp 4</td>
+						</tr>
+						<tr>
+							<td class="topic">Passengers :  </td>
+							<td class="descrip">&nbsp 5</td>
+						</tr>
+						
+						<tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1820kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 5064mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>		
+			
+		</div>
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<?php include "footer.php" ?>
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+</body>
+
+</html>

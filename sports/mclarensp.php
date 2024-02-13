bebcbbc83093da796643925de46dcde340c737fa
Onaliy Vinukiy Jayawardana <ovjayawardana@students.nsbm.ac.lk>
2024-02-13 18:53:28 +0530

    Create mclarensp.php

---
+++ sports/mclarensp.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Mclaren Sport Cars</title>
+<link rel="icon" type="image/x-icon" href="/image/Drivex.png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="DriveX">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../styles/bootstrap4/bootstrap.min.css">
+<link href="../plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="../plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="../styles/classiccars.css">
+<link rel="stylesheet" type="text/css" href="../styles/about_responsive.css">
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+<?php include "header.php"?>
+
+</header>
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="container">
+		
+				</div>
+				
+					<div class="intro_content">
+						<div class="intro_title"><center>Mclaren Sport Cars</center></div>
+						
+						<div><div></div><span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</div>
+<!-- Start blog Area -->
+<section class="blog-area section-gap" id="blog">
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/McLarenP11.jpg">
+				<ul class="post-tags">
+					<li>$7,000,000</li>
+					<li>$2020</li>
+					<li>England</li>
+					
+				</ul>
+				<h4 class="headline">Mclaren P1</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.8L V8</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3799cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp900NM@7500RPM</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 903bhp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp350km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp2.4s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Carbonfibre Monocoque</td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1395kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4588mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>					
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/Mclaren720s.jpg">
+				<ul class="post-tags">
+					<li>$301,500</li>
+					<li>2022</li>
+					<li>England</li>
+					
+				</ul>
+				<h4 class="headline">Mclaren 720S</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp4.0L 8V</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3994cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp560Nm@5500rpm</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 710hp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 341km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp2.8s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Fetherlight Carbonfibre </td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1431kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4543mm</td>
+						</tr>
+					</table>
+				</p>
+                <br><br>
+				
+			</div>						
+			<div class="col-lg-7 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/McLaren570s.jpg">
+				<ul class="post-tags">
+					<li>$395,000</li>
+					<li>2022</li>
+					<li>England</li>
+					
+				</ul>
+				<h4 class="headline">McLaren 570S</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp3.8L V8</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3799cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp600NM@5000rpm</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Diesel</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 562hp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 328km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp3.1s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Fetherlight Carbonfibre </td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1399kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4530mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+			
+			
+			<div class="col-lg-5 col-md-5 single-blog">
+				<img class="img-fluid" src="../image/Mclarengt.jpg">
+				<ul class="post-tags">
+					<li>$215,000</li>
+					<li>2022</li>
+					<li>England</li>
+					
+				</ul>
+				<h4 class="headline">Mclaren GT</h4>
+				<p>
+					<table>
+						
+						<tr>
+							<td class="topic">Engine : </td>
+							<td td class="descrip">&nbsp&nbsp4.0l V8</td>
+						</tr>
+
+						<tr>
+							<td class="topic">Engine Capacity : </td>
+							<td td class="descrip">&nbsp&nbsp3994cc</td>
+						</tr>
+
+                        <tr>
+							<td class="topic">Torque : </td>
+							<td td class="descrip">&nbsp&nbsp630Nm@5500rpm</td>
+						</tr>
+                        
+                        <tr>
+							<td class="topic">Fuel Type :  </td>
+							<td class="descrip">&nbsp Petrol</td>
+						</tr>
+						<tr>
+							<td class="topic">Transmission :  </td>
+							<td class="descrip">&nbsp 7 Speed Automatic</td>
+						</tr>
+						<tr>
+							<td class="topic">Power :  </td>
+							<td class="descrip">&nbsp 612hp</td>
+						</tr>
+						<tr>
+							<td class="topic">Top Speed :  </td>
+							<td class="descrip">&nbsp 327km/h</td>
+						</tr>
+                        <tr>
+							<td class="topic">Acceleration 0-100 km/h : </td>
+							<td td class="descrip">&nbsp&nbsp3.1s</td>
+						</tr>
+                        <tr>
+							<td class="topic">Chassis :  </td>
+							<td class="descrip">&nbsp Titanium & Carbonfibre </td>
+						</tr>
+                        <tr>
+							<td class="topic">No of Doors : </td>
+							<td td class="descrip">&nbsp&nbsp2</td>
+						</tr>
+                        <tr>
+							<td class="topic">Weight :  </td>
+							<td class="descrip">&nbsp 1530kg</td>
+						</tr>
+						<tr>
+							<td class="topic">Length :  </td>
+							<td class="descrip">&nbsp 4683mm</td>
+						</tr>
+					</table>
+				</p>
+				<br><br>
+			</div>
+		</div>
+	</div>	
+</section>
+<!-- End blog Area -->
+<!-- Footer -->
+	
+<footer>
+    <div class="footer-top text-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 text-center">
+                    <h4 class="navbar-brand">DRIVEX</h4>
+                    <p>We provide information about all types of cars in the world to all car addictors. We provide information about classic cars, luxury cars, sports cars and new arrivals. Also we have a range of articles about cars as well as our social media pages provide you additional insights about all types of cars.</p>
+                    <div class="col-auto social-icons">
+                        <a href="#"><i class='fa fa-facebook-f'></i></a>
+                        <a href="#"><i class='fa fa-twitter'></i></a>
+                        <a href="#"><i class='fa fa-instagram'></i></a>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom text-center">
+        <p class="mb-0"></p> Made With <i class="fa fa-heart-o" aria-hidden="true"></i> By ICONS
+            
+    </div>
+</footer>
+
+
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../styles/bootstrap4/popper.js"></script>
+<script src="../styles/bootstrap4/bootstrap.min.js"></script>
+<script src="../plugins/greensock/TweenMax.min.js"></script>
+<script src="../plugins/greensock/TimelineMax.min.js"></script>
+<script src="../plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="../plugins/greensock/animation.gsap.min.js"></script>
+<script src="../plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="../plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="../plugins/easing/easing.js"></script>
+<script src="../plugins/parallax-js-master/parallax.min.js"></script>
+<script src="../js/about_custom.js"></script>
+
+
+</body>
+
+</html>
